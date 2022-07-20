801f03843faac07d8b5eb3e1078f5f26f0b407f8
duon97ran2 <duongtaph13276@fpt.edu.vn>
2022-07-20 14:11:51 +0700

    update cors

--- src/index.js
+++ src/index.js
@@ -19,8 +19,14 @@ const fileUpload = multer();
 app.use(morgan("tiny"));
 app.use(express.json({ limit: "50mb" }));
 dotenv.config({ path: __dirname + "/configs/settings.env" });
+const corsOptions = {
+  origin: "*",
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+};
 
-app.use(cors());
+
+app.use(cors(corsOptions));
 
 cloudinary.config({
   cloud_name: "duongtaph13276",
