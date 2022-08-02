64270d950dbd927f5e839502f1f030c903c738a2
duon97ran2 <duongtaph13276@fpt.edu.vn>
2022-08-02 14:05:40 +0700

    update user model

--- src/model/users.js
+++ src/model/users.js
@@ -24,6 +24,9 @@ const userSchema = mongoose.Schema({
     type: Array,
     default: ['https://mdbootstrap.com/img/new/avatars/1.webp']
   },
+  phone: {
+    type: String,
+  },
   username: {
     type: String
   },
